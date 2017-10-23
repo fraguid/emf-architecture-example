e8c699388452fcdafdf90018c96523cacb08a689
Vincenzo Caselli <vincenzo.caselli@rcp-vision.com>
2017-10-23 08:03:55 +0200

    Changed style for dirty objects

--- it.rcpvision.ecf2017.example.ui/src/it/rcpvision/ecf2017/example/ui/custom/DirtyRowsTableLabelProvider.java
+++ it.rcpvision.ecf2017.example.ui/src/it/rcpvision/ecf2017/example/ui/custom/DirtyRowsTableLabelProvider.java
@@ -1,8 +1,9 @@
 package it.rcpvision.ecf2017.example.ui.custom;
 
 import org.eclipse.emf.parsley.ui.provider.TableColumnLabelProvider;
-import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
@@ -15,14 +16,24 @@ public class DirtyRowsTableLabelProvider extends TableColumnLabelProvider {
 	@Inject
 	EmfUtil emfUtil;
 
-	Color GREEN= new Color(Display.getCurrent(),new RGB(0, 250, 0));
+	Color RED= new Color(Display.getCurrent(),new RGB(255, 0, 0));
 	
 	@Override
-	public Color getBackground(Object element) {
+	public Color getForeground(Object element) {
 		if (emfUtil.isDirty(element)) {
-			return GREEN;
+			return RED;
 		} else {
-			return super.getBackground(element);
+			return super.getForeground(element);
 		}
 	}
+	
+	@Override
+	public Font getFont(Object element) {
+		if (emfUtil.isDirty(element)) {
+			return JFaceResources.getFontRegistry().getBold(JFaceResources.DEFAULT_FONT);
+		} else {
+			return JFaceResources.getFontRegistry().get(JFaceResources.DEFAULT_FONT);
+		}
+	}
+	
 }
